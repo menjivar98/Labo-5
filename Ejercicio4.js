b959493c882515a42f9a15657b4abf2974e7a89d
Fernando Menjivar <00195316>
2018-09-21 03:01:44 -0600

    Segundo commit

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -2,14 +2,8 @@ var decimal = prompt("Ingrese el numero decimal");
 convertir(decimal);
 
 function convertir(n){
-    var contador;
-    var operacion;
+    var num = n;
 
-    while(n >= 0){
-       operacion = n/2;
-       contador = operacion%2 + contador;     
-    }
-
-    console.log(contador);
+    alert( num.toString(2) ); 
 
 }
